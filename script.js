4e0fcb56a8e3db5b19de2aad661c4e95bdfd69bc
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-01-03 14:04:24 -0300

    Construindo js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const item = document.getElementById('input-item').value;
+const item = document.getElementById('input-item');
 const salvarItem = document.getElementById('adicionar-botao');
+const listaDeCompras = document.getElementById('lista-de-compras');
+let contador = 0;
 
 salvarItem.addEventListener("click", adicionarItem);
 
@@ -10,6 +12,67 @@ function adicionarItem(evento) {
     const containerItemLista = document.createElement("div");
     containerItemLista.classList.add("item-lista-container");
     
+    const containerNomeDoItem = document.createElement("div");
+    containerNomeDoItem.classList.add("container-nome-compra");
 
+    const containerCheckbox = document.createElement("div");
+    containerCheckbox.classList.add("checkbox-container");
+
+    const checkboxInput = document.createElement("input");
+    checkboxInput.type = "checkbox";
+    checkboxInput.classList.add("checkbox-input");
+    checkboxInput.id = "checkbox-" + contador++;
+
+    const checkboxLabel = document.createElement("label");
+    checkboxLabel.setAttribute("for", checkboxInput.id);
+
+    checkboxLabel.addEventListener("click", function(evento) {
+        const checkboxInput = evento.currentTarget.querySelector(".checkbox-input");
+        const checkboxCustomizado = evento.currentTarget.querySelector(".checkbox-customizado");
+
+        if (checkboxInput.checked) {
+            checkboxCustomizado.classList.add("checked");
+        } else {
+            checkboxCustomizado.classList.remove("checked");
+        }
+    })
+
+    const checkboxCustomizado = document.createElement("div");
+    checkboxCustomizado.classList.add("checkbox-customizado");
+
+    checkboxLabel.appendChild(checkboxInput);
+    checkboxLabel.appendChild(checkboxCustomizado);
+
+    containerCheckbox.appendChild(checkboxLabel);
+    containerNomeDoItem.appendChild(containerCheckbox);
+
+    const NomeDoItem = document.createElement("p");
+    NomeDoItem.innerText = item.value;
+    containerNomeDoItem.appendChild(NomeDoItem);
+
+    const containerBotoes = document.createElement("div");
+    containerBotoes.classList.add("container-botao-acao");
+    const botaoRemover = document.createElement("button");
+    botaoRemover.classList.add("botao-acao");
+    const botaoEditar = document.createElement("button");
+    botaoEditar.classList.add("botao-acao");
+
+    const imagemRemover = document.createElement("img");
+    imagemRemover.src = "img/delete.svg";
+    imagemRemover.alt = "Remover";
+
+    const imagemEditar = document.createElement("img");
+    imagemEditar.src = "img/edit.svg";
+    imagemEditar.alt = "Editar";
+
+    botaoRemover.appendChild(imagemRemover);
+    containerBotoes.appendChild(botaoRemover);
+
+    botaoEditar.appendChild(imagemEditar);
+    containerBotoes.appendChild(botaoEditar);
+
+    containerItemLista.appendChild(containerNomeDoItem);
+    containerItemLista.appendChild(containerBotoes);
     itemDaLista.appendChild(containerItemLista);
+    listaDeCompras.appendChild(itemDaLista);
 }
